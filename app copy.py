cc1264253f887952620360c7a879612ccaec6b40
Sam <samandarusmanov1@gmail.com>
2025-05-15 20:33:18 +0500

    fix

---
+++ app copy.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='../static', static_url_path='/static', template_folder='../templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Vercel ищет переменную `app` или `handler`
+handler = app
