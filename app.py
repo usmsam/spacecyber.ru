701f212c861d07c8f9ccfd4d5ed90a25a2b12847
Sam <samandarusmanov1@gmail.com>
2025-05-15 20:47:58 +0500

    fix

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='../static', static_url_path='/static', template_folder='../templates')
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Vercel ищет переменную `app` или `handler`
-handler = app
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=4500)
